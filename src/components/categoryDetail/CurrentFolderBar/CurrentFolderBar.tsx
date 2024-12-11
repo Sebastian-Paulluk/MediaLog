cccf0b83100003a024a71c77b70746d78d842cec
Sebastian-Paulluk <129323586+Sebastian-Paulluk@users.noreply.github.com>
2024-12-11 10:17:12 -0300

    Modifico estilo del current folder bar

--- src/components/categoryDetail/CurrentFolderBar/CurrentFolderBar.tsx
+++ src/components/categoryDetail/CurrentFolderBar/CurrentFolderBar.tsx
@@ -24,7 +24,6 @@ export const CurrentFolderBar: React.FC<CurrentFolderBarTypes> = ({handleToggleO
 
     const getFoldersInCategory =()=> categoryId ? getFoldersByCategoryId(categoryId).length : 0;
 
-
     return (
         <div className='cf-bar'>
             <div className='cf-bar__content'>
@@ -33,27 +32,42 @@ export const CurrentFolderBar: React.FC<CurrentFolderBarTypes> = ({handleToggleO
 
                     <div className='cf-bar__content__body__left' onClick={handleToggleOpenFoldersMenu}>
                         <div className='cf-bar__content__body__left__open-folders-button'>
-                            <div className='cf-bar__content__body__left__open-folders-button__img-container'>
+                            <div className='cf-bar__content__body__left__open-folders-button__arrow-img-container'>
                                 <img
                                     src={arrowImg}
                                     alt='arrow'
                                     className={
-                                        `cf-bar__content__body__left__open-folders-button__img-container__img
+                                        `cf-bar__content__body__left__open-folders-button__arrow-img-container__img
                                         ${openFoldersMenu ? 'open-cf-bar': ''}`
                                     } 
                                 />
                             </div>
-                            <div className='cf-bar__content__body__left__open-folders-button__folders-text'>
-                                Folders
+
+                            <div className={
+                                `cf-bar__content__body__left__open-folders-button__folder-count
+                                ${getCurrentFolderName() !== 'Root' ? 'not-root' : ''}`
+                            }>
+                                <div className='cf-bar__content__body__left__open-folders-button__folder-count__img-container'>
+                                
+                                    <img src={folderImg} className='ghost-folder' alt='ghost-folder-container__img' />
+                                    <img
+                                        src={folderImg}
+                                        className='cf-bar__content__body__left__open-folders-button__folder-count__img-container__img'
+                                        alt='folder-img'
+                                    />
+
+
+                                </div>
+
+                                <div className='cf-bar__content__body__left__open-folders-button__folder-count__number'>
+                                    {getFoldersInCategory()}
+                                </div>
                             </div>
+                        
                         </div>
 
                         <div className='cf-bar__content__body__left__cf-text'>
-                            Current
-                            <span className='cf-bar__content__body__left__cf-text__folder'>
-                                folder
-                            </span>
-                            :  
+                            Current:  
                         </div>
                     </div>
 
@@ -65,14 +79,7 @@ export const CurrentFolderBar: React.FC<CurrentFolderBarTypes> = ({handleToggleO
                     </div>
 
                     <div className='cf-bar__content__body__right'>
-                        <div className='cf-bar__content__body__right__folder-count'>
-                            <div className='cf-bar__content__body__right__folder-count__img-container'>
-                                <img src={folderImg} className='cf-bar__content__body__right__folder-count__img-container__img' alt='folder-img' />
-                            </div>
-                            <div className='cf-bar__content__body__right__folder-count__number'>
-                                {getFoldersInCategory()}
-                            </div>
-                        </div>
+
                     </div>
                 </div>
 
